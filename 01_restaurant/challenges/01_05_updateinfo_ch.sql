3527449447b9779517271cb30a9ef0bfd5ccb7e7
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-08-30 13:01:12 +0000

    01-05

--- 01_restaurant/challenges/01_05_updateinfo_ch.sql
+++ 01_restaurant/challenges/01_05_updateinfo_ch.sql
@@ -2,3 +2,19 @@
 
 -- Taylor Jenkins aus 27170 6th Ave., Washington, DC,
 -- ist umgezogen nach 74 Pine St., New York, NY.
+
+UPDATE Kunden
+SET Adresse = '74 Pine St.',
+    Stadt = 'New York',
+    State = 'NY'
+WHERE KundenID = (SELECT KundenID
+                  FROM Kunden
+                  WHERE Vorname = 'Taylor' 
+                  AND Nachname = 'Jenkins' 
+                  AND Stadt = 'Washington'
+                  AND State = 'DC')
+                  ;
+
+SELECT *
+FROM Kunden
+WHERE Vorname = 'Taylor' AND Nachname = 'Jenkins';
