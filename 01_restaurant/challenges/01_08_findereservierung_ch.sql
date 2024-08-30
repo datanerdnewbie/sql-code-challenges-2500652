886147182f02ba119560cb0283138362173ef0d5
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-08-30 14:49:22 +0000

    01_08

--- 01_restaurant/challenges/01_08_findereservierung_ch.sql
+++ 01_restaurant/challenges/01_08_findereservierung_ch.sql
@@ -4,3 +4,12 @@
 -- Der Name hat unter anderem folgenede Variationen:
 -- Stevensen, Stephensen, Stevenson, Stephenson, Stuyvesant
 -- Heute ist der 14.Juni.
+
+Select CONCAT(k.Vorname, ' ' ,k.Nachname) AS Name,
+        r.Datum,
+        r.PartyGroesse
+FROM Reservierung AS r
+JOIN Kunden AS k
+  ON k.KundenID = r.KundenID
+WHERE k.Nachname LIKE ('St%')
+Order By r.Datum DESC;
