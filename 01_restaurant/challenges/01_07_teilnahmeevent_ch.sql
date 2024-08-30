276cebe10a16f0d628d3f9190f527e846a0c3e79
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-08-30 13:25:00 +0000

    01_07

--- 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
+++ 01_restaurant/challenges/01_07_teilnahmeevent_ch.sql
@@ -1,3 +1,15 @@
--- Registrieren Sie einen Kunden 'atapley2j@kinetecoinc.com' 
+-- Registrieren Sie einen Kunden 'atapley2j@kinetecoinc.com'
 -- für unsere Jubilaeumsfeier.
 -- Der Kunde wird mit drei Freunden an der Feier teilnehmen.
+
+SELECT KundenID
+FROM Kunden
+WHERE Email = 'atapley2j@kinetecoinc.com';
+
+INSERT INTO Jubilaeum(KundenID, PartyGroesse)
+VALUES ((SELECT KundenID
+          FROM Kunden
+          WHERE Email = 'atapley2j@kinetecoinc.com'),
+          4);
+
+SELECT * FROM Jubilaeum;
