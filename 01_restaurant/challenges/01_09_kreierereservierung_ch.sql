03e65c6e589f8c1cd691376a29d095e58a8daf9d
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-08-30 15:10:45 +0000

    01_09

--- 01_restaurant/challenges/01_09_kreierereservierung_ch.sql
+++ 01_restaurant/challenges/01_09_kreierereservierung_ch.sql
@@ -4,3 +4,24 @@
 -- Die Reservierung ist für:
 -- Sam McAdams (smac@kinetecoinc.com), 5 Personen
 -- August 12, 2022 @ 6PM (18:00)
+-- Telefon: 555-555-1212
+
+SELECT *
+From Kunden
+WHERE Email = 'smac@kinetecoinc.com'
+;
+
+Insert Into Kunden(Vorname, Nachname, Email, Telefon)
+Values ('Sam', 'McAdams', 'smac@kinetecoinc.com', '555-555-1212')
+;
+
+Insert Into Reservierung (KundenID, Datum, PartyGroesse)
+Values ((SELECT KundenID FROM Kunden WHERE Email = 'smac@kinetecoinc.com'),
+        '2022-08-12 18:00:00',
+        5)
+;
+
+SELECT *
+From Reservierung 
+WHERE KundenID = (SELECT KundenID FROM Kunden WHERE Email = 'smac@kinetecoinc.com')
+;
