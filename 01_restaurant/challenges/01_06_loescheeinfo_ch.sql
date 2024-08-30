2ea3849dc6c71324b66fe769b474ce55b7a065b4
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-08-30 13:13:32 +0000

    01_06

--- 01_restaurant/challenges/01_06_loescheeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_loescheeinfo_ch.sql
@@ -2,3 +2,13 @@
 
 -- Ein Kunde namens Norby hat uns informiert, dass er seine Reservierung
 -- für Freitag stornieren moechte. Heute ist der 24.Juli 2022.
+
+SELECT * 
+FROM Reservierung
+WHERE KundenID = (SELECT KundenID
+                  FROM Kunden
+                  WHERE Vorname = 'Norby');
+
+SELECT *
+FROM Reservierung
+WHERE Datum >= '2022-07-24';
