b48c0c422e15dbac0ddb13a337b03ebc083b665a
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-02 08:53:19 +0000

    1_10

--- 01_restaurant/challenges/01_10_eintragbestellung_ch.sql
+++ 01_restaurant/challenges/01_10_eintragbestellung_ch.sql
@@ -5,3 +5,68 @@
 -- Für: Loretta Hundey, 6939 Elka Place, 
 -- Im Einkaufskorb: 1 House Salad, 1 Mini Cheeseburgers, 1 Tropical Blue Smoothie,
 -- Lieferdatum und Zeit: September 20, 2022, @2PM (14:00)
+
+-- Bestellung in Bestellungen erstellen
+Insert Into Bestellungen(KundenID, BestellungsDatum)
+Values((SELECT KundenID
+        FROM Kunden
+        WHERE Vorname = 'Loretta'
+        AND Nachname = 'Hundey'
+        AND Adresse = '6939 Elka Place'
+        ),
+        '2022-09-20 14:00:00'
+        )
+;
+
+-- Die Bestellung in BestellungSpeise erstellen
+WITH Bestellung AS(
+SELECT BestellungsID
+FROM Bestellungen
+WHERE BestellungsDatum = '2022-09-20 14:00:00'
+  AND KundenID = (SELECT KundenID
+                  FROM Kunden
+                  WHERE Vorname = 'Loretta'
+                    AND Nachname = 'Hundey'
+                    AND Adresse = '6939 Elka Place')
+)
+INSERT INTO BestellungSpeise(BestellungsID, SpeiseID)
+Values((SELECT BestellungsID
+        FROM Bestellung),
+        (SELECT SpeiseID
+         FROM Speise
+         WHERE Name = 'House Salad')
+      ),((SELECT BestellungsID
+          FROM Bestellung),
+          (SELECT SpeiseID
+            FROM Speise
+            WHERE Name = 'Mini Cheeseburgers')
+        ),((SELECT BestellungsID
+            FROM Bestellung),
+            (SELECT SpeiseID
+              FROM Speise
+              WHERE Name = 'Tropical Blue Smoothie')
+          )
+;
+
+-- Abfrage der Gesamtkosten mit CTE
+WITH BestellteSpeisen AS(
+SELECT SpeiseID
+FROM BestellungSpeise
+WHERE BestellungsID = (SELECT BestellungsID
+                      FROM Bestellungen
+                      WHERE BestellungsDatum = '2022-09-20 14:00:00'
+                      AND KundenID = (SELECT KundenID
+                                      FROM Kunden
+                                      WHERE Vorname = 'Loretta'
+                                      AND Nachname = 'Hundey'
+                                      AND Adresse = '6939 Elka Place')
+                      )
+)
+
+SELECT SUM(Preis) AS Gesamtkosten
+FROM Speise
+WHERE SpeiseID IN (SELECT * FROM BestellteSpeisen)
+;
+
+
+
