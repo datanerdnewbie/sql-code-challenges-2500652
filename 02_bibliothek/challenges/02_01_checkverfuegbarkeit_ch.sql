373672ceecc89644af8b626c5dd9a0643130adff
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-02 09:50:45 +0000

    02-01

--- 02_bibliothek/challenges/02_01_checkverfuegbarkeit_ch.sql
+++ 02_bibliothek/challenges/02_01_checkverfuegbarkeit_ch.sql
@@ -1,3 +1,18 @@
 -- Finden Sie heraus wie viele Kopien von 
 -- „Dracula“ im Moment verfügbar sind.
 
+WITH GesamtAnzahlExemplare AS(
+SELECT Titel, BuchID, COUNT(*) AS AnzahlGesamt
+FROM Buecher
+WHERE Titel = 'Dracula'
+GROUP BY Titel
+)
+
+SELECT (SELECT Titel 
+        FROM GesamtAnzahlExemplare) AS Buchtitel,
+        (SELECT AnzahlGesamt 
+          FROM GesamtAnzahlExemplare) - COUNT(*) AS VerfügbareExemplare
+FROM Ausleihung 
+WHERE BuchID = (SELECT BuchID FROM GesamtAnzahlExemplare)
+ AND RueckgabeDatum IS NULL
+;
