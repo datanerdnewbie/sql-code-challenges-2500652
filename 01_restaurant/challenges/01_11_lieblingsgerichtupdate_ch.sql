9bd1a8f271060dffff24c8d4220dd9a2d24d996b
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-02 09:25:20 +0000

    01_11

--- 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
+++ 01_restaurant/challenges/01_11_lieblingsgerichtupdate_ch.sql
@@ -2,3 +2,19 @@
 
 -- Tragen Sie Cleo Goldwaters Lieblingsgericht 
 -- als "Quinoa Salmon Salad" ein.
+
+--Tabelle Kunden aktualiesieren
+UPDATE Kunden
+SET Lieblingsgericht = (SELECT SpeiseID
+                        FROM Speise
+                        WHERE Name = 'Quinoa Salmon Salad')
+WHERE Vorname = 'Cleo' AND Nachname = 'Goldwater'
+;
+
+-- Kontrolle
+SELECT k.Vorname, k.Nachname, sp.Name AS Lieblingsgericht
+FROM Kunden AS k
+JOIN (SELECT SpeiseID, Name FROM Speise) AS sp
+  ON k.Lieblingsgericht = sp.SpeiseID 
+WHERE k.Vorname = 'Cleo' AND k.Nachname = 'Goldwater'
+;
--- 01_restaurant/challenges/01_12_bestekunden_ch.sql
+++ 01_restaurant/challenges/01_12_bestekunden_ch.sql
@@ -4,3 +4,19 @@
 
 -- Geben Sie die Anzahl der Bestellungen, 
 -- den Vornamen, Nachnamen sowie die Email-Adresse an.
+
+-- Liste an Kunden dessen Bestellungsrang < 15 ist
+WITH AnzahlBestellungen AS(
+SELECT KundenID, 
+        COUNT(*) AS Anzahl,
+        RANK() OVER(ORDER BY COUNT(*) DESC) AS Rang
+FROM Bestellungen
+GROUP BY KundenID
+)
+
+SELECT k.Vorname, k.Nachname, k.Email, ab.Anzahl 
+FROM Kunden AS k
+JOIN AnzahlBestellungen AS ab
+ ON k.KundenID = ab.KundenID
+WHERE ab.Rang <= 15
+;
