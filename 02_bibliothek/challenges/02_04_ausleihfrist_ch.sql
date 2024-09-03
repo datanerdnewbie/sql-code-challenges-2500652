d09eb1c3e4f00c9a198ef8f5053532fb1268d8ad
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-03 09:07:12 +0000

    02_04

--- 02_bibliothek/challenges/02_04_ausleihfrist_ch.sql
+++ 02_bibliothek/challenges/02_04_ausleihfrist_ch.sql
@@ -1,3 +1,23 @@
 -- Erstellen Sie eine Liste von Büchern, deren Ausleihfrist am 13. Juli 2022 endet. 
 -- Diese Liste soll auch den Namen der ausleihenden Person, 
 -- sowie deren Email-Adresse enthalten. 
+
+SELECT a.Rueckgabefrist, 
+        b.Titel, 
+        b.Barcode, 
+        CONCAT(m.Vorname, ' ', m.Nachname) AS Name, 
+        m.Email
+FROM Ausleihung AS a
+LEFT JOIN (SELECT MitgliedsID, 
+                  Vorname, 
+                  Nachname, 
+                  Email 
+            FROM Mitglieder) AS m
+  ON a.MitgliedsID = m.MitgliedsID
+LEFT JOIN (SELECT BuchID, 
+                  Titel, 
+                  Barcode 
+            FROM Buecher) AS b
+  ON a.BuchID = b.BuchID
+WHERE Rueckgabefrist = '2022-07-13'
+;
