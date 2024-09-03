4dd3073d01582eb3014a82608e82c10e100b42ea
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-03 11:26:17 +0000

    02_05

--- 02_bibliothek/challenges/02_05_buchrueckgabe_ch.sql
+++ 02_bibliothek/challenges/02_05_buchrueckgabe_ch.sql
@@ -5,3 +5,15 @@
 -- Buch 1: 6435968624
 -- Buch 2: 5677520613
 -- Buch 3: 8730298424
+
+UPDATE Ausleihung
+SET Rueckgabedatum = '2022-07-05'
+WHERE BuchID = (SELECT BuchID 
+                FROM Buecher 
+                WHERE Barcode IN (6435968624,
+                                  5677520613,
+                                  8730298424
+                                 )
+                )
+ AND Rueckgabedatum IS NULL
+;
