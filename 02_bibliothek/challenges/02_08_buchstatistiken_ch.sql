06da0a2abd0e0f6b0412ad6907b3d2df9caada80
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-03 12:27:29 +0000

    02_08

--- 02_bibliothek/challenges/02_08_buchstatistiken_ch.sql
+++ 02_bibliothek/challenges/02_08_buchstatistiken_ch.sql
@@ -2,3 +2,19 @@
 
 -- Die erste soll zeigen wie viele Bücher pro Jahr veröffentlicht wurden. 
 -- Die zweite soll zeigen, welche fünf Bücher am meisten ausgeliehen worden sind.
+
+SELECT Erscheinungsdatum, 
+        Count(DISTINCT(Titel)) AS Anzahl
+FROM Buecher
+Group BY Erscheinungsdatum
+ORDER BY Anzahl DESC, Erscheinungsdatum
+;
+
+SELECT COUNT(*) AS Anzahl, b.Titel, b.Barcode
+FROM Ausleihung as a
+JOIN Buecher AS b
+ ON a.BuchID = b.BuchID
+Group By b.Titel
+Order By Anzahl DESC
+LIMIT 5
+;
