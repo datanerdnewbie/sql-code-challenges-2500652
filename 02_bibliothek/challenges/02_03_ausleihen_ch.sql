2dabad7582802e9be16491112a2582645f7fbb18
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-03 08:53:30 +0000

    02_03

--- 02_bibliothek/challenges/02_03_ausleihen_ch.sql
+++ 02_bibliothek/challenges/02_03_ausleihen_ch.sql
@@ -2,3 +2,57 @@
 --  und „Great Expectations“ an Jack Vaan aus. 
 -- Das Ausleihdatum ist der 25. August 2022  
 --  und die Ausleihfrist endet am 8. September.
+
+WITH Kundennummer AS(
+SELECT MitgliedsID
+FROM Mitglieder
+WHERE Vorname = 'Jack' AND Nachname = 'Vaan'
+),
+VerlieheneBuecher AS(
+SELECT b.BuchID, a.RueckgabeDatum
+FROM Buecher AS b
+JOIN Ausleihung AS a
+  ON b.BuchID = a.BuchID
+WHERE (b.Titel = 'The Picture of Dorian Gray' 
+  OR b.Titel = 'Great Expectations')
+  AND a.RueckgabeDatum IS NULL
+),
+Buchnummer1 AS(
+SELECT BuchID
+FROM Buecher
+WHERE Titel = 'The Picture of Dorian Gray'
+  AND BuchID NOT IN (SELECT BuchID
+                      FROM VerlieheneBuecher
+                    )
+LIMIT 1
+),
+Buchnummer2 AS(
+SELECT BuchID
+FROM Buecher
+WHERE Titel = 'Great Expectations'
+  AND BuchID NOT IN (SELECT BuchID
+                      FROM VerlieheneBuecher
+                    )
+LIMIT 1
+)
+
+INSERT INTO Ausleihung(BuchID, MitgliedsID, Ausleihdatum, Rueckgabefrist)
+VALUES ((SELECT BuchID FROM Buchnummer1),
+         (SELECT MitgliedsID FROM Kundennummer), 
+         '2022-08-25', '2022-09-08'),
+        ((SELECT BuchID FROM Buchnummer2),
+          (SELECT MitgliedsID FROM Kundennummer),
+         '2022-08-25', '2022-09-08')
+;
+
+SELECT a.AusleihID, a.MitgliedsID, a.Ausleihdatum, b.Titel
+FROM Ausleihung AS a
+JOIN (SELECT BuchID, Titel FROM Buecher) AS b
+  ON a.BuchID = b.BuchID 
+WHERE a.MitgliedsID = (SELECT MitgliedsID
+                      FROM Mitglieder
+                      WHERE Vorname = 'Jack' 
+                      AND Nachname = 'Vaan'
+                    )
+  AND a.Ausleihdatum = '2022-08-25'
+;
