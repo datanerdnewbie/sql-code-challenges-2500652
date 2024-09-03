4b843fdca83a3bfde1092ed607556ccfbb055409
Andreas Langer <143646179+datanerdnewbie@users.noreply.github.com>
2024-09-03 11:51:17 +0000

    02_06

--- 02_bibliothek/challenges/02_06_seltenebesucher_ch.sql
+++ 02_bibliothek/challenges/02_06_seltenebesucher_ch.sql
@@ -1,2 +1,15 @@
 -- Erstellen Sie eine Liste von Mitgliedern,  
 -- die am seltensten Bücher ausgeliehen haben. 
+
+SELECT m.Email
+FROM Ausleihung AS a
+LEFT JOIN (SELECT MitgliedsID, Email FROM Mitglieder) AS m
+  ON a.MitgliedsID = m.MitgliedsID
+GROUP BY a.MitgliedsID
+HAVING COUNT(a.MitgliedsID) < AVG((SELECT COUNT(*)
+                                    FROM Ausleihung
+                                    GROUP BY MitgliedsID
+                                  )
+                                  )
+ORDER BY COUNT(a.MitgliedsID)
+;
